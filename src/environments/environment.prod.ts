eea29d8f83caedd2079687abf4ec349cc8537976
Katerina <kateclysm@gmail.com>
2023-03-10 19:46:15 -0300

    Configuración provisoria del componente inicio, ng build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  //URL: 'http://.............'
+  URL: 'https://bemariamonchot.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
   production: false,
   URL: 'http://localhost:8080/'
   //URL: 'https://bemariamonchot.onrender.com'
